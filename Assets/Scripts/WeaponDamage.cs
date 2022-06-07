5295787a0ac7cbdeff7dbde765a837ed6a98d592
jahirock <jahirock@live.com.mx>
2022-06-07 17:58:38 -0600

    Added sword with collider to the player object. Modified the player animations to move the sword.

---
+++ Assets/Scripts/WeaponDamage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    public int damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
+        }
+    }
+}
